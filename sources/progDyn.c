2c754ff9ac0351fe4a61f488d2fac2616f1cb251
sabouazz <sabouazz@gmail.com>
2012-05-03 23:27:30 +0200

    ajout prog dynamique de partition compilée et testée

--- sources/progDyn.c
+++ sources/progDyn.c
@@ -13,9 +13,9 @@ int max(int a, int b){
 }
 
 
-//algo en lui-même
+//algo en lui-meme
 
-int sacADos(int W, int* poids, int* utilite, int nbObjets){ //W représente la capacite max du sac
+int sacADos(int W, int* poids, int* utilite, int nbObjets){ //W represente la capacite max du sac
 
 // tableau central de la programmation dynamique
 int tab[nbObjets][W+1]; 
@@ -39,13 +39,48 @@ for (w = 0; w < W + 1; w++){
     }
   }
 //la valeur qui nous interesse
- printf(" poids: %d \n", W);
- printf("meilleure solution (utilite): %d \n", tab[nbObjets -1] [W]);
+ printf(" poids : %d \n", W);
+ printf("meilleure solution (utilite) : %d \n", tab[nbObjets -1] [W]);
  return (tab[nbObjets -1] [W]);
 }
 
 
 /* Partition */
+
+int partition(int* poids, int nbObjets){ 
+
+
+//calcul de la somme totale des poids
+ int i, j;
+int N = 0;
+for(i = 0; i < nbObjets; i++ ) {N += poids[i];}
+
+int tab[N]; 
+
+//initialisation de la table de booleens
+
+ tab[0] = 1;
+ for(i = 1; i <= N; i ++ )  {tab[i] = 0;}
+
+
+//remplissage de la table selon les formules de la prog dyn
+
+ for(i = 0; i < nbObjets; i++ ){
+   for(j = N - poids[i]; j >= 0; j-- ){
+     if( tab[j] == 1 ) {
+       tab[j + poids[i]] = 1;
+     }
+   }
+ }
+
+//la reponse qui nous interesse
+ if(tab[N / 2] == 1){ 
+   printf("vrai avec %d \n", N/2);
+   }
+ else{puts("faux");}
+
+ return tab[N / 2];
+}
 
 /* Voyageur de commerce */
 
@@ -71,6 +106,17 @@ int main(){
 
 /* instance partition */
 
+  int nPart = 2;
+  int pPart[nPart];
+  
+  pPart[0] = 5; pPart[1] = 5; 
+  
+  puts("debut resolution partition");
+  
+  partition(pPart, nPart);
+
+  puts("fin resolution partition");
+  
 /* instance voyageur de commerce */
 
 }
