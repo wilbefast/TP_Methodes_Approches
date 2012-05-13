36a78af8310bae86730b630c0681ec56920d1fa9
sabouazz <sabouazz@gmail.com>
2012-05-14 00:59:58 +0200

    branch and bound écrit, mais pas compilé ni testé

--- projet/branchBound/bbCaml.ml
+++ projet/branchBound/bbCaml.ml
@@ -1,29 +1,52 @@
 open Graph.Pack.Graph
-
+  
 (* fonctions de comparaison *)
-
+  
 let compare f a b = f a - f b
 
-let compareValeur = compare int
+let compareLabel f a b = f (label a) - f (label b)  
 
+let compareValeur = compare int
+  
 let min l = List.hd (List.sort compareValeur l)
 
 	 (* relie x à ses deux voisins les plus proches en terme de coût*)
-	 let relier x g =
+  
+	 let relier x g gbis=
+	   let listAretes x g = succ_e x g in
+	   let listTriee = List.sort compareLabel listAretes in
+	   let arete1 = List.hd listTriee in
+	   let arete2 = List.1th listTriee in
+	   add_edge_e gbis arete1
+	   add_edge_e gbis arete2
+	    
+	   
+(* fonction qui somme les éléments d'une liste d'entiers *)
+let rec somme = List.fold_left (+) 0
 
-(* calcule la valeur d'un cycle *)
-
-	   let valeurCycle g =
+	   (* calcule la valeur d'un cycle *)
+  
+	 let valeurCycle g = somme (map label List.append(map_vertex succ_e v g))
+	   
+	     (* vérifie si un sommet v est de degré supérieur 2 *)
+	   
+	     let sup2 v g = out_degree g v > 2
 
 (* vérifie si tous les sommets sont de degré 2 *)
 
-	     let tousDegre2 g =
+	     let tousDegre2 g = 
+	       let listeBool = map_vertex sup2 g in
+	       not (exists (fun x -> true) listeBool)
+
+(* renvoi un sommet de degré > 2 s'il existe *)
+
+	     let sommetSup2 v g = if out_degree g v > 2 then v else () 	       
 
 (* choisi un sommet de degré > 2 *)
 
-	       let choixSup2 g = 
+	       let choixSup2 g = List.hd (map_vertex sommetSup2 g)
 
-(* fonction qui enlève une arete e d'un graphe et fait le branchement dessus à partir de x*)
+(* fonction qui enlève une arete e d'un graphe et fait le branchement dessus à partir de x *)
 
 		 let rec branche e g x borneInf solution = 
 		   remove_edge_e g e
@@ -33,9 +56,10 @@ let min l = List.hd (List.sort compareValeur l)
 (* algorithme de branch and bound en lui même *)
 		     
 		 and  branchBound g x borneInf solution = 
+		   let ginit = copy g
 		   remove_vertex g x
 	 let arbre = spanningtree g in
-	 let cycle = relier x g in
+	 let cycle = relier x ginit g in
 	 let val = valeurCycle cycle in
 	 if val > borneInf then 
 	   if tousDegre2 cycle then
@@ -46,7 +70,7 @@ let min l = List.hd (List.sort compareValeur l)
 	     let listeSolutions = map branche aretesSucc in
 	 else()
 	   solution = min listeSolutions
-	     return solution
+    
 	     
 
 
