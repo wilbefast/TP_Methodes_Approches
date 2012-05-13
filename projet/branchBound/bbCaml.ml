1af89b9dca3ea1083aae232e7e15b50819d085c5
sabouazz <sabouazz@gmail.com>
2012-05-13 23:22:07 +0200

    debut de branch and bound en caml

--- projet/branchBound/bbCaml.ml
+++ projet/branchBound/bbCaml.ml
@@ -1,26 +1,44 @@
-Require : un sommet racine, une borne inférieure, un graphe G
-1 : faire ACPM G − {x}
-2 : relier x à ses deux voisins les plus proches en terme de coût
-3 : if valeur du cycle > borne inférieure then
-4:
-if tous les sommets sont de degré 2 then
-5:
-mettre à jour le solution courante
-6:
-else
-7:
-choisir un sommet y de degré ≥ 2
-8:
 for chaque arête ei issue de y do
 9:
 G′ := G − {ei }
 10 :
 BranchAndBound(y, borne inférieure, G′ )
-11 :
-end for
-12 :
-end if
-13 : end if
-14 : retourner la plus petite des solutions acceptables
 
-	 open 
+	 open Graph.Pack.Graph
+
+	 (* relie x à ses deux voisins les plus proches en terme de coût*)
+	 let relier x g =
+
+(* calcule la valeur d'un cycle *)
+
+	   let valeurCycle g =
+
+(* vérifie si tous les sommets sont de degré 2 *)
+
+	     let tousDegre2 g =
+
+(* choisi un sommet de degré > 2 *)
+
+	       let choixSup2 g = 
+
+(* algorithme de branch and bound en lui même *)
+
+	       let rec branchBound g x borneInf solution = 
+	     remove_vertex g x
+	 let arbre = spanningtree g in
+	 let cycle = relier x g in
+	 let val = valeurCycle cycle in
+	 if val > borneInf then 
+	   if tousDegre2 cycle then
+	     solution = val
+	   else
+	     let y = choixSup2 g in
+
+	 else()
+	   
+	   return solution
+
+
+
+
+	   
