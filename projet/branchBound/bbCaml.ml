7a440f86db57c8f0b0fa5c81b50e2cf122512d32
sabouazz <sabouazz@gmail.com>
2012-05-15 09:55:26 +0200

    modifs ds le code caml du branch and bound, mais ne compile toujours pas

--- projet/branchBound/bbCaml.ml
+++ projet/branchBound/bbCaml.ml
@@ -23,9 +23,11 @@ let min l = List.hd (List.sort compareValeur l)
 	   let listTriee = List.sort compareLabel listAretes in
 	   let arete1 = List.hd listTriee in
 	   let arete2 = List.hd (List.tl listTriee) in
-	   add_edge_e gbis arete1
-	   add_edge_e gbis arete2
-	    
+	   let graphe = create in
+	   fold add_edge_e graphe gbis
+	     add_edge_e graphe arete1
+	     add_edge_e graphe arete2
+	     
 	   
 (* fonction qui somme les éléments d'une liste d'entiers *)
 let rec somme = List.fold_left (+) 0
@@ -53,39 +55,39 @@ let rec somme = List.fold_left (+) 0
 
 (* algorithme de branch and bound en lui même *)
 		     
-	       let rec branchBound g x borneInf solution = 
+	     let rec branchBound g x borneInf solution = 
 	       let g' = copy g in
 	       remove_vertex g x
-	       let arbre = spanningtree g in;; 
-	       let cycle = relier x g' g in ;;
-	       let val = valeurCycle cycle in;;
-	       if val > borneInf then 
-		 if tousDegre2 cycle then
-		   solution = val
-		 else
-		   let y = choixSup2 g in
-		   let aretesSucc = succ_e g y in 
-		   let listeSolutions = map branche aretesSucc in
-	       else()
-		 solution = min listeSolutions
-		   
+	     let cycle = bellman_ford (relier x g'(spanningtree g)) x in
+	     let valeur = valeurCycle cycle in
+	     if valeur > borneInf then 
+	       if tousDegre2 cycle then
+		 solution = valeur
+	       else
+		 let y = choixSup2 g in
+		 let aretesSucc = succ_e g y in 
+		 let listeSolutions = map branche aretesSucc in
+	     else()
+	       solution = min listeSolutions
+		 
+		 
+	     (* fonction qui enlève une arete e d'un graphe et fait le branchement dessus à partir de x *)
+		 
+	     and branche e g x borneInf solution = 
+	       remove_edge_e g e;;
+	     branchBound g x borneInf solution;;
 	     
-(* fonction qui enlève une arete e d'un graphe et fait le branchement dessus à partir de x *)
-
-	       and branche e g x borneInf solution = 
-		 remove_edge_e g e;;
-	       branchBound g x borneInf solution;;
-
-
-(*
- * Les tests !
+	     
+	     (*
+	      * Les tests !
  *)
-
-let g = Rand.graph ~v:10 ~e:20 ()
-let () = show g
-let beforeBB = Sys.time ()
-let_ = branchBound g 3 1 1
-let afterBB = Sys.time ()
-let _ =
-    Printf.printf   "BranchBound : %f\n"
-                    (afterBB -. beforeBB)
+	     
+	     let g = Rand.graph ~v:10 ~e:20 ()
+	     let () = show g
+	     let beforeBB = Sys.time ()
+	       let_ = branchBound g 3 1 1
+	     let afterBB = Sys.time ()
+	     let _ =
+	       Printf.printf   "BranchBound : %f\n"
+                 (afterBB -. beforeBB)
+		 
