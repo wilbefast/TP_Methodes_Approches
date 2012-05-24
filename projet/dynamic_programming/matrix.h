df4bf564c25884deb993c4150d8ab3aa9b2676c1
william <wilbefast@gmail.com>
2012-05-24 16:53:17 +0300

    removed unused typedef and declarations

--- projet/dynamic_programming/matrix.h
+++ projet/dynamic_programming/matrix.h
@@ -7,21 +7,6 @@
 
 int matrix_unit();
 
-// cell structure : a row-column pair
-typedef struct
-{
-  size_t row, col;
-}
-cell_t;
-
-// valued index : an index-value pair
-typedef struct
-{
-  int value;
-  size_t index;
-}
-valued_index_t;
-
 // matrix structure
 typedef struct
 {
@@ -37,8 +22,5 @@ void free_matrix(matrix_t* m);
 
 // print to standard output
 void print_matrix(matrix_t* m);
-
-// query
-valued_index_t min_col(matrix_t* m, size_t row);
 
 #endif // MATRIX_H_INCLUDED
